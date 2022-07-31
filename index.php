b5dc1e17845351b7948d4ab258adb023496c0358
ion_t_dev <iontulburedev@gmail.com>
2022-08-01 00:43:41 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+<?php
+// Silence is golden.
+=======
+<?php // Silence is golden
+>>>>>>> 16f5bd6a1b029c2db9ab4b99dbd837fe16f9b799
---
+++ rocket-books/admin/class-rocket-books-admin.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Rocket_Books
+ * @subpackage Rocket_Books/admin
+ */
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Rocket_Books
+ * @subpackage Rocket_Books/admin
+ * @author     Your Name <email@example.com>
+ */
+class Rocket_Books_Admin {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $rocket_books    The ID of this plugin.
+	 */
+	private $rocket_books;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $rocket_books       The name of this plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $rocket_books, $version ) {
+
+		$this->rocket_books = $rocket_books;
+		$this->version = $version;
+
+	}
+
+	/**
+	 * Register the stylesheets for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Rocket_Books_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Rocket_Books_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->rocket_books, plugin_dir_url( __FILE__ ) . 'css/rocket-books-admin.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Rocket_Books_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Rocket_Books_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script( $this->rocket_books, plugin_dir_url( __FILE__ ) . 'js/rocket-books-admin.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+}
